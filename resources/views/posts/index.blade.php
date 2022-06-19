9d5ceed06a6c35dc1f5962cb59a624ca7b164ca2
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2022-06-19 13:29:34 +0300

    I corrected a mistake in the URL of the show.post file.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -15,7 +15,7 @@
                 @foreach ($posts as $post)
                     <div class="blog-post mb-3">
                         <h2 class="blog-post-title">
-                            <a href="posts/{{ $post->id }}">{{ $post->title }}</a>
+                            <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
                         </h2>
                         <p class="blog-post-meta">بقلم
                             <a href="#">{{ $post->author }}</a>
